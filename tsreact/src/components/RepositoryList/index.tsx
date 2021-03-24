d170529df042feabdfe5ced87e05f4838b60a6a6
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-03-24 14:53:10 -0300

    creating interfaces

---
+++ tsreact/src/components/RepositoryList/index.tsx
@@ -0,0 +1,30 @@
+import React,{Component} from 'react';
+
+interface Repository{
+    id:number;
+    name: string;
+}
+
+interface Props{
+    repositories:Repository[];
+}
+
+interface State{
+    newRepository?: string
+}
+
+export default class RepositoryList extends Component<Props, State> {
+    state = {
+        newRepository:'',
+    }
+
+    render() {
+        const {repositories} = this.props 
+        const { newRepository} = this.state
+        return (
+            <ul>
+                {repositories.map(repository => <li key={repository.id}>{repository.name}</li>)}
+            </ul>
+        )
+    }
+}
